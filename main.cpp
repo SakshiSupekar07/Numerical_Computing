b78e1901a4bbf59da90c373cfd6f90a96316ebe6
Supekar <supekarsakshj07@gmail.com>
2025-02-07 11:52:16 +0530

    RootFinding commit

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include "RootFinding.hpp"
+
+using namespace std;
+
+
+
+int main()
+{
+     RootFinding solver;
+
+    double a, b, tole;
+    int max_iter;
+
+   
+    cout << "Enter interval [a, b]: ";
+    cin >> a >> b;
+    cout << "Enter tolerance: ";
+    cin >> tole;
+    cout << "Enter maximum iterations: ";
+    cin >> max_iter;
+
+    
+    double rootbisection = solver.bisection(a, b, max_iter, tole);
+
+    
+        cout << "\nApproximate root: " << rootbisection << "\n";
+   
+
+   
+    double x0, tol;
+    int iter;
+    RootFinding rf;
+    cout << "Enter initial value for newton raphson: ";
+    cin >> x0;
+    cout << "Enter tolerance: ";
+    cin >> tol;
+    cout << "Enter max iterations: ";
+    cin >> iter;
+    double root = rf.newtonRaphson(x0, tol, iter);
+    cout << "\nEstimated Root: " << root << "\n";
+
+    double x1, tolerance;
+    int iterations;
+    RootFinding rootf;
+    cout << "Enter initial point: ";
+    cin >> x1;
+    cout << "Enter tolerance: ";
+    cin >> tolerance;
+    cout << "Enter max iterations: ";
+    cin >> iterations;
+    double rootfixed = rootf.fixedpoint(x1, tolerance, iterations);
+    cout << "\nEstimated Root: " << rootfixed << "\n";
+
+
+
+
+    return 0;
+}
